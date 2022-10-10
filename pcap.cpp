4b8e4a967e6cc8291d58503a033192c589fd4658
Tomas Bartu <tomasbartu00@gmail.com>
2022-10-11 00:01:38 +0200

    parsing packets values to map with touples

--- pcap.cpp
+++ pcap.cpp
@@ -1,10 +1,18 @@
-//
-// Created by bartu on 6.10.22.
-//
+/*****************************************************************************
+ * Soubor: pcap.cpp
+ *
+ * Popis: Zachytávání a analýza zachycené síťové komunikace
+ *
+ * Autor: Tomáš Bártů, xbartu11
+ *
+ * Datum: 7.10.2022
+ *****************************************************************************/
 
+#include <iostream>
 #include "pcap.h"
 
-#include <sys/sysinfo.h>
+// srcIP, dstIP, sport, dport, prot
+std::map<tuple<string, string, int, int, int>, tuple<in_addr>> m;
 
 void pcapInit(options options) {
     pcap_t *handle = nullptr;
@@ -23,23 +31,51 @@ void pcapInit(options options) {
     printf("Netflow finished\n");
 }
 
-void p_time(time_t time, suseconds_t m_second) {
-    char buffer[512];
-    char z_buffer[6];                              /* časová zóna je ve formátu (+0100) plus '\0' => 6  */
-    struct tm *recv_time = localtime(&time); /* pomocí localtime získáme strukturu, obsahující dny, roky, měsíce */
-    // data ze struktury pomocí funkce strftime, uložíme do bufferů v požadovaném formátu
-    strftime(buffer, sizeof(buffer), "%FT%T", recv_time);
-    strftime(z_buffer, sizeof(z_buffer), "%z", recv_time);
-    // vypíšeme čas z bufferů a přidáme k t tomu i milivteřiny
-    printf("timestamp: %s.%ld%s\n", buffer, m_second, z_buffer);
+string p_ip(const struct ip *ip_header, int type) {
+    /* pomocí inet_ntoa() vypíšeme adresy ze síťového prostředí (uložená v bajtech) na dekadickou tečkovou notaci */
+    if (type == SOURCE)
+        return inet_ntoa(ip_header->ip_src);
+    else if (type == DESTINATION)
+        return inet_ntoa(ip_header->ip_dst);
+    else
+        err(EXIT_FAILURE, "Undefined error in p_ip()");
 }
+
+string p_port_tcp(const struct tcphdr *tcp_header) {
+    /* Protože číslo portu je uloženo v tzv. "network byte order", tak ho převedeme na tzv. "host byte order" */
+    printf("src port: %d\n", ntohs(tcp_header->th_sport));
+    printf("dst port: %d\n", ntohs(tcp_header->th_dport));
+}
+
+string p_port_udp(const struct udphdr *udp_header) {
+    /* Protože číslo portu je uloženo v tzv. "network byte order", tak ho převedeme na tzv. "host byte order" */
+    printf("src port: %d\n", ntohs(udp_header->uh_sport));
+    printf("dst port: %d\n", ntohs(udp_header->uh_dport));
+}
+
+void print_map() {
+    for (auto it = m.cbegin(); it != m.cend(); it++) {
+        cout << "->" << get<0>(it->first) << " <- \n";
+    }
+    std::cout << '\n';
+}
+
 
 void handler(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes) {
 
-    // TODO h->ts.tv_sec
-    p_time((time_t) h->ts.tv_sec, h->ts.tv_usec);
-
     auto *eth_header = (struct ether_header *) bytes;
+
+    printf("ts: %ld [ms]\n", timeval_to_ms(h->ts));
+
+
+
+    /*auto pos = m.find(make_tuple(2, "HAHA"));
+    if (pos == m.end()) {
+        cout << "Neni :)" << '\n';
+    } else {
+        cout << pos->second << '\n';
+    }*/
+
 
     if(ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
         auto *ip_header = (struct ip *) (bytes + ETH_HLEN);
@@ -49,15 +85,33 @@ void handler(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes) {
         if(ip_header->ip_p == IPPROTO_ICMP) {
             auto *icmp_header = (struct icmphdr *) (bytes + ip_len + ETH_HLEN);
 
+            cout << p_ip(ip_header, SOURCE) << '\n';
+            //cout << p_ip(ip_header, DESTINATION) << '\n';
+            auto search = m.find(make_tuple(p_ip(ip_header, SOURCE), p_ip(ip_header, DESTINATION), 0, 0, icmp_header->type));
+            if (search != m.end()) {
+                std::cout << "Founded" << '\n';
+            } else {
+                std::cout << "Not found\n";
+                m.insert(make_pair(make_tuple(p_ip(ip_header, SOURCE), p_ip(ip_header, DESTINATION), 0, 0, icmp_header->type), ip_header->ip_src));
+            }
+
         }
 
         if(ip_header->ip_p == IPPROTO_TCP) {
             auto *tcp_header = (struct tcphdr *) (bytes + ip_len + ETH_HLEN);
+            p_port_tcp(tcp_header);
         }
 
         if(ip_header->ip_p == IPPROTO_UDP) {
             auto *udp_header = (struct udphdr *) (bytes + ip_len + ETH_HLEN);
-
+            p_port_udp(udp_header);
         }
     }
+    print_map();
 }
+
+unsigned long timeval_to_ms(struct timeval ts) {
+    return ((ts.tv_usec + 500) / 1000) + (ts.tv_sec * 1000);
+}
+
+/************** Konec souboru pcap.cpp ***************/
--- pcap.h
+++ pcap.h
@@ -1,6 +1,13 @@
-//
-// Created by bartu on 6.10.22.
-//
+/*****************************************************************************
+ * Soubor: pcap.h
+ *
+ * Popis: Hlavičkový soubor pro zachytávání a analýza zachycené síťové
+ * komunikace
+ *
+ * Autor: Tomáš Bártů, xbartu11
+ *
+ * Datum: 7.10.2022
+ *****************************************************************************/
 
 #ifndef ISA_PROJECT_PCAP_H
 #define ISA_PROJECT_PCAP_H
@@ -12,12 +19,25 @@
 #include <netinet/ip.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
+#include <map>
+#include <tuple>
+#include <string>
+#include <string_view>
 #include "arguments.h"
 
+using namespace std;
+
+#define SOURCE 1
+#define DESTINATION 2
 
 void pcapInit(options);
 
 void handler(u_char *, const struct pcap_pkthdr *, const u_char *);
 
+unsigned long timeval_to_ms(struct timeval);
+
 #endif //ISA_PROJECT_PCAP_H
+
+/************** Konec souboru pcap.h ***************/
